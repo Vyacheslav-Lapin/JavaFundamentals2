db3a487e387685e9cfecc0d8805c387fbd111f9d
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-06-24 02:38:17 +0300

    CurrentThreadDemo

---
+++ threads/src/main/java/CurrentThreadDemo.java
@@ -0,0 +1,16 @@
+public class CurrentThreadDemo {
+    public static void main(String[] args) {
+        Thread t = Thread.currentThread();
+        System.out.println("Текущий поток: " + t);
+        t.setName("My Thread");
+        System.out.println("После изменения имени: " + t);
+        try {
+            for (int n = 5; n > 0; n--) {
+                System.out.println(n);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.err.println("Exception: " + e);
+        }
+    }
+}
