b6413cb50f96bbc84a08d44529e3514aac69d01d
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-06-24 02:58:05 +0300

    Demo join

---
+++ threads/src/main/java/NewThread.java
@@ -0,0 +1,45 @@
+public class NewThread implements Runnable {
+    private Thread t;
+    public NewThread(String threadName) {
+        t = new Thread(this, threadName);
+        System.out.println("New thread: " + t);
+        t.start(); // Start the thread
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println(t.getName() + ": " + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(t.getName() + " interrupted.");
+        }
+        System.out.println(t.getName() + " exiting.");
+    }
+
+    public static void main(String[] args) {
+        NewThread ob1 = new NewThread("One");
+        NewThread ob2 = new NewThread("Two");
+        NewThread ob3 = new NewThread("Three");
+
+        System.out.println("Thread One is alive: " + ob1.t.isAlive());
+        System.out.println("Thread Two is alive: " + ob2.t.isAlive());
+        System.out.println("Thread Three is alive: " + ob3.t.isAlive());
+
+        try {
+            System.out.println("Waiting for threads to finish.");
+            ob1.t.join();
+            ob2.t.join();
+            ob3.t.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main thread Interrupted");
+        }
+
+        System.out.println("Thread One is alive: " + ob1.t.isAlive());
+        System.out.println("Thread Two is alive: " + ob2.t.isAlive());
+        System.out.println("Thread Three is alive: " + ob3.t.isAlive());
+        System.out.println("Main thread exiting.");
+    }
+}
