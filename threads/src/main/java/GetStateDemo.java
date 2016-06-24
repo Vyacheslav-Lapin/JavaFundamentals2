d32ec97cce380afe3203999fee8ef9a460563c2c
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-06-24 03:01:40 +0300

    Demo Thread States

---
+++ threads/src/main/java/GetStateDemo.java
@@ -0,0 +1,22 @@
+public class GetStateDemo implements Runnable {
+
+    @Override
+    public void run() {
+        Thread.State state
+                = Thread.currentThread().getState();
+        System.out.println(Thread.currentThread().getName()
+                + " " + state);
+    }
+
+    public static void main(String[] args) {
+        Thread th1 = new Thread(new GetStateDemo());
+        th1.start();
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        Thread.State state = th1.getState();
+        System.out.println(th1.getName() + " " + state);
+    }
+}
