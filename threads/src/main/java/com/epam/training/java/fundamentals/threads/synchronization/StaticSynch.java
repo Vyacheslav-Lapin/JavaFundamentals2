f212a5b45f52a303d492319ac6dbbbffa88b86ea
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-06-25 23:48:03 +0300

    Synchronize static Demo

---
+++ threads/src/main/java/com/epam/training/java/fundamentals/threads/synchronization/StaticSynch.java
@@ -0,0 +1,33 @@
+package com.epam.training.java.fundamentals.threads.synchronization;
+
+public class StaticSynch {
+
+    public static synchronized void a() {
+        System.out.println("Line #1 in the method a");
+        try {
+            Thread.sleep(1_000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Line #2 in the method a");
+    }
+
+    public static synchronized void b() {
+        System.out.println("Line #1 in the method b");
+        try {
+            Thread.sleep(1_000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Line #2 in the method b");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable r = () -> { a(); b(); };
+        Thread[] threads = { new Thread(r), new Thread(r), new Thread(r), new Thread(r) };
+
+        for (Thread thread: threads)
+            thread.start();
+    }
+
+}
